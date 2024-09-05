c7f795a409cbc0cad04e273bf70f0de5c64aa0b5
mohamed eid <85568105+mohamed3idm68@users.noreply.github.com>
2024-09-05 22:37:48 +0300

    update reminder app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import { Component } from "react";
+import { Component, useRef } from "react";
 import "./App.css";
 import { connect } from "react-redux";
-
 
 import {
   add_reminder,
@@ -12,17 +11,16 @@ import moment from "moment";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-
-
-
 class App extends Component {
   state = {
     text: "",
     date: new Date(),
+    remindernotify: '',
+    timeOutId: null,
   };
 
 
-  
+  minDate = new Date()
 
   rendeRreminder = () => {
     const { reminders } = this.props;
@@ -34,7 +32,9 @@ class App extends Component {
             <>
               <li key={reminder.id} className="list-group-item">
                 <div className="text">{reminder.text}</div>
-                <div className="date">{moment(new Date(reminder.date)).fromNow()}</div>
+                <div className="date">
+                  {moment(new Date(reminder.date)).fromNow()}
+                </div>
                 <div
                   className="remove btn btn-danger"
                   onClick={() => this.props.remove_reminder(reminder.id)}
@@ -50,67 +50,69 @@ class App extends Component {
   };
 
   render() {
-    console.log(this.props)
-    return ( 
+    console.log(this.props);
+    return (
       <>
-      
-      <div className="App">
+        <div className="App">
           <div className="reminder-title">
             <h1>What Should you DO ?</h1>
           </div>
-          <input
-            type="text"
-            placeholder="enter what do you think"
-            max-length="20"
-            className="form-control"
-            onChange={(e) => this.setState({ text: e.target.value })}
-            value={this.state.text}
-          />
-          {/* <input
+          <form>
+            <input
+              type="text"
+              placeholder="enter what do you think"
+              max-length="20"
+              className="form-control"
+              onChange={(e) => this.setState({ text: e.target.value })}
+              value={this.state.text}
+              required
+            />
+            {/* <input
             type="datetime-local"
             className="form-control"
             onChange={(e) => this.setState({ date: e.target.value })}
             value={this.state.date}
           /> */}
-          <DatePicker
-           className="form-control"
-            placeholderText="select time"
-            value={this.state.date}
-            selected={this.state.date}
-            onChange={(date) => this.setState({ date: date })}
-            showTimeSelect
-            timeFormat="HH:mm"
-            dateFormat="MMMM d, yyy h:mm aa"
-            timeCaption="time"
-          />
-          <button
-            className="btn btn-primary btn-block"
-
-            onClick={() => {
-              this.props.add_reminder(this.state.text, this.state.date);
-              this.setState({ text: "", date: "" });
-            }}
-          >
-            Add Reminder
-          </button>
-          {this.rendeRreminder()}
-          <button
-            className="btn btn-danger btn-block "
-            onClick={() => this.props.clear_reminder()}
-          >
-            Clear Reminder
-          </button> 
-       </div>
-   </>
-
-    )
-     
-  }}
-    
-      
-     
-
-
+            <DatePicker
+              className="form-control"
+              placeholderText="select time"
+              value={this.state.date}
+              selected={this.state.date}
+              onChange={(date) => this.setState({ date: date })}
+              showTimeSelect
+              timeFormat="HH:mm"
+              dateFormat="MMMM d, yyy h:mm aa"
+              timeCaption="time"
+              minDate={this.minDate}
+              required
+            />
+            <button
+              className="btn btn-primary btn-block"
+              type="submit"
+              onClick={() => {
+                if (this.state.text.trim() === "") {
+                  return "";
+                } else {
+                  this.props.add_reminder(this.state.text, this.state.date);
+                  this.setState({ text: "", date: "" });
+                }
+              }}
+            >
+              Add Reminder
+            </button>
+            {this.rendeRreminder()}
+            <button
+              className="btn btn-danger btn-block "
+              onClick={() => this.props.clear_reminder()}
+            >
+              Clear Reminder
+            </button>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
 
 export default connect(
   (state) => {
@@ -120,6 +122,3 @@ export default connect(
   },
   { add_reminder, remove_reminder, clear_reminder }
 )(App);
-
-
-
